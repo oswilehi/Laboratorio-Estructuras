492f15a764879fe1b906d58213923a016a4e9ef6
oswilehi <oswilehi@gmail.com>
2017-02-25 23:06:00 -0600

    Started the Node and BinaryTree class

---
+++ Lab02-JoseAlvarez-OscarLemus/Lab02-JoseAlvarez-OscarLemus/Extras/BinaryTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab02_JoseAlvarez_OscarLemus.Extras
+{
+    public class BinaryTree<T>
+    {
+        private Node<T> root;
+        private int element_count;
+
+        public BinaryTree()
+        {
+            root = null;
+            element_count = 0;
+        }
+
+        public void Insert(T Data)
+        {
+            if (root == null)
+            {
+                root = new Node<T>(Data);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ Lab02-JoseAlvarez-OscarLemus/Lab02-JoseAlvarez-OscarLemus/Extras/Node.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab02_JoseAlvarez_OscarLemus.Extras
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> left { get; set; }
+        public Node<T> right { get; set; }
+
+        public Node(T data_)
+        {
+            this.Data = data_;
+        }
+
+        public void insertData(ref Node<T> node, T data)
+        {
+            if (node == null)
+            {
+                node = new Node<T>(data);
+
+            }
+            //else if (node.Data < data)
+            //{
+            //    insertData(ref node.right, data);
+            //}
+
+            //else if (node.Data > data)
+            //{
+            //    insertData(ref node.left, data);
+            //}
+        }
+    }
+}
