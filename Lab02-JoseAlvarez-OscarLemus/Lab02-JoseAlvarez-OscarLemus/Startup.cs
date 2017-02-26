b3e95233e3fe8c1fc983812f1efbbc17b1ff2e1c
oswilehi <oswilehi@gmail.com>
2017-02-25 22:50:22 -0600

    Create project

---
+++ Lab02-JoseAlvarez-OscarLemus/Lab02-JoseAlvarez-OscarLemus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab02_JoseAlvarez_OscarLemus.Startup))]
+namespace Lab02_JoseAlvarez_OscarLemus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
